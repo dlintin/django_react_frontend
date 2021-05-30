d130a89298d1b5e4604db2c282dab10bd9ca83e5
dlintin <dev.dianto.lintin@gmail.com>
2021-05-31 02:38:42 +0800

    fix endpoint url

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
 
   refreshList = () => {
     axios
-      .get("https://dlintin-django-react-backend.zeet.app/api/todos/")
+      .get("/api/todos/")
       .then((res) => this.setState({ todoList: res.data }))
       .catch((err) => console.log(err));
   };
@@ -37,18 +37,18 @@ class App extends Component {
 
     if (item.id) {
       axios
-        .put(`https://dlintin-django-react-backend.zeet.app/api/todos/${item.id}/`, item)
+        .put(`/api/todos/${item.id}/`, item)
         .then((res) => this.refreshList());
       return;
     }
     axios
-      .post("https://dlintin-django-react-backend.zeet.app/api/todos/", item)
+      .post("/api/todos/", item)
       .then((res) => this.refreshList());
   };
 
   handleDelete = (item) => {
     axios
-      .delete(`https://dlintin-django-react-backend.zeet.app/api/todos/${item.id}/`)
+      .delete(`/api/todos/${item.id}/`)
       .then((res) => this.refreshList());
   };
 
